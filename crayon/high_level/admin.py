a027eb1ed1da87bb777400221b4a6eb39ca8d8c8
Chakir Boulahyia <chak34500@gmail.com>
2024-09-25 16:51:18 +0200

    add high_level models & admin

--- crayon/high_level/admin.py
+++ crayon/high_level/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Ville)
+admin.site.register(models.Siege_social)
+admin.site.register(models.Machine)
+admin.site.register(models.Usine)
+admin.site.register(models.Ressource)
+admin.site.register(models.Quantite_Ressource)
+admin.site.register(models.Etape)
+admin.site.register(models.Stock)
+admin.site.register(models.Produit)
--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -7,4 +7,83 @@ class Ville(models.Model):
     prix_m2 = models.IntegerField()
 
     def __str__(self):
-        return f"Nom: {self.nom}"
+        return f"{self.nom}"
+
+
+class Machine(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.IntegerField()
+    n_serie = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nom}"
+
+
+class Local(models.Model):
+    nom = models.CharField(max_length=100)
+    ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
+    surface = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nom}"
+
+    class Meta:
+        abstract = True
+
+
+class Siege_social(Local):
+    def __init(self):
+        pass
+
+
+class Usine(Local):
+    machines = models.ManyToManyField(Machine)
+
+
+class Objet(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nom}"
+
+    class Meta:
+        abstract = True
+
+
+class Ressource(Objet):
+    def __init(self):
+        pass
+
+
+class Quantite_Ressource(models.Model):
+    ressource = models.ForeignKey(Ressource, on_delete=models.PROTECT)
+    quantite = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.ressource}"
+
+
+class Etape(models.Model):
+    nom = models.CharField(max_length=100)
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    quantite_ressource = models.ForeignKey(Quantite_Ressource, on_delete=models.PROTECT)
+    duree = models.IntegerField()
+    etape_suivante = models.ForeignKey("self", null=True, on_delete=models.CASCADE)
+
+    ##null=True permet de rendre optionnel etape suivante
+    ##Models.CASCADE permets de detruire,
+    ##ex si on detruit le produit on detruit les etape, c'est equivalent a
+    # composition forte et faible. (forte models.CASCADE ,faible models.PROTECT)
+
+
+class Produit(Objet):
+    premiere_etape = models.ForeignKey(Etape, null=True, on_delete=models.PROTECT)
+
+
+class Stock(models.Model):
+    objet = models.ForeignKey(Ressource, on_delete=models.PROTECT)
+    nombre = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.objet}"
